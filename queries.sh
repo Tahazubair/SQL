4fc573cee159cf196741ca11b0b948300bf3e2b8
Muhammad Taha Zubair Khan <70602969+Tahazubair@users.noreply.github.com>
2022-08-02 21:35:11 +0500

    Update queries.sh

--- queries.sh
+++ queries.sh
@@ -29,13 +29,13 @@ echo -e "\nWinner of the 2018 tournament team name:"
 echo "$($PSQL "SELECT name FROM teams INNER JOIN games ON teams.team_id = games.winner_id WHERE round='Final' AND year=2018 GROUP BY name")"
 
 echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
-echo "$($PSQL "SELECT name FROM teams LEFT JOIN games ON teams.team_id = games.winner_id OR teams.team_id = games.game_id WHERE round='Eighth-Final' AND year=2014 GROUP BY name")"
+echo "$($PSQL "SELECT name FROM teams LEFT JOIN games ON teams.team_id = games.winner_id OR teams.team_id = games.opponent_id WHERE round='Eighth-Final' AND year=2014 ORDER BY name")"
 
 echo -e "\nList of unique winning team names in the whole data set:"
 echo "$($PSQL "SELECT DISTINCT(name) FROM teams RIGHT JOIN games ON teams.team_id = games.winner_id ORDER BY name")"
 
 echo -e "\nYear and team name of all the champions:"
-echo "$($PSQL "SELECT year,name FROM teams RIGHT JOIN games ON teams.team_id = games.winner_id WHERE round='FINAL'")"
-
+echo "$($PSQL "SELECT year,name FROM teams RIGHT JOIN games ON teams.team_id = games.winner_id WHERE round='Final' ORDER BY year")"
 echo -e "\nList of teams that start with 'Co':"
 echo "$($PSQL "SELECT name FROM teams WHERE name ILIKE 'Co%'")"
+
